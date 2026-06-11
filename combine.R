69623382774b6fc424285d0faed6e72422eb62b6
krlong68 <krlong586@gmail.com>
2026-06-11 13:55:52 -0700

    add dummy rows, adjust dates used for sunrise times

--- combine.R
+++ combine.R
@@ -29,25 +29,48 @@ sbat_list <- lapply(sonobat_txt_files, function(x) {
 
 # Format like XLSX
 
-wpz_coords <- matrix(c(-122.35083578760666, 47.66851544625889), nrow = 1)
+#lon, lat
+lon <- -122.35083578760666
+lat <- 47.66851544625889
+wpz_coords <- matrix(c(lon, lat), nrow = 1)
 tz <- "America/Los_Angeles"
 
-create_zoo_data <- function(sonobat_df, tz, crds) {
+create_zoo_data <- function(sonobat_df, tz, lon, lat) {
+    # Format coordinates for suntools::sunriset
+    crds <- matrix(c(lon, lat), nrow = 1)
+    
     zdf <- sonobat_df %>%
+        mutate(night_date = parse_date_time(MonitoringNight,
+                                            orders = c("mdy", "ymd"),
+                                            tz = tz)) %>%
+        complete(night_date = seq.POSIXt(rollback(night_date[1],
+                                                  roll_to_first = TRUE),
+                                         rollforward(night_date[1]),
+                                         by = "DSTday"),
+                 fill = list(SppAccp = "NoRec",
+                             HiF = "x",
+                             LoF = "x")) %>%
+        mutate(MonitoringNight = if_else(is.na(MonitoringNight),
+                                         strftime(night_date, "%-m/%-d/%Y"),
+                                         MonitoringNight),
+               Timestamp = if_else(is.na(Timestamp),
+                                   sub("(..)$", ":\\1", strftime(night_date +
+                                                                     days(1),
+                                                "%Y-%m-%dT%H:%M:%OS6%z")),
+                                   Timestamp)) %>%
         mutate(posix = parse_date_time(Timestamp, "YmdHMOSz")) %>%
         mutate(lposix = with_tz(posix, tz)) %>%
         mutate(Month = month(lposix, label = TRUE),
                Date = date(lposix),
-               Time = format(lposix, "%I:%M:%S %p"),
-               night_date = parse_date_time(MonitoringNight,
-                                            orders = c("mdy", "ymd"),
-                                            tz = tz)) %>%
+               Time = strftime(lposix, "%I:%M:%S %p")) %>%
         mutate(Sunset = sunriset(crds, night_date, direction = "sunset",
                                  POSIXct.out = TRUE)$time,
-               Sunrise = sunriset(crds, night_date, direction = "sunrise",
+               Sunrise = sunriset(crds, night_date + days(1),
+                                  direction = "sunrise",
                                   POSIXct.out = TRUE)$time,
-               `Elapsed (hr)` = time_length(interval(Sunset, lposix),
-                                            "hour")) %>%
+               `Elapsed (hr)` = if_else(SppAccp == "NoRec", NA,
+                                        time_length(interval(Sunset, lposix),
+                                                    "hour"))) %>%
         mutate(Sunset = format(Sunset, "%I:%M:%S %p"),
                Sunrise = format(Sunrise, "%I:%M:%S %p")) %>%
         mutate(SppAccp = case_when(SppAccp == "x" ~ NA, .default = SppAccp),
@@ -60,30 +83,52 @@ create_zoo_data <- function(sonobat_df, tz, crds) {
         rename(Temp = `Temperature Int`)
 }
 
-zd_list <- lapply(sbat_list, function(x) create_zoo_data(x, tz, wpz_coords))
+zd_list <- lapply(sbat_list, function(x) create_zoo_data(x, tz, lon, lat))
 
-# Combine months, add dummy rows, and arrange data
-noaa_station <- noaa_nearby(wpz_coords[2], wpz_coords[1])$station[1]
-
+# Combine months and arrange data
 zd_df <- bind_rows(zd_list) %>%
+    distinct() %>%
+    group_by(MonitoringNight) %>%
+    mutate(ind = (n() > 1 & Species == "NoRec")) %>%
+    filter(!ind) %>%
+    ungroup() %>%
     group_by(Month) %>%
     mutate(`Month Ind` = case_when(posix == min(posix) ~ 
                                        strftime(posix, format = "%b"),
                                    .default = "")) %>%
     ungroup() %>%
     arrange(posix) %>%
-    
-    select(Month, Date, Time, Sunrise, Sunset, `Elapsed (hr)`, Species,
-           Timestamp, Temp, HiF, LoF, `Month Ind`)
+    select(Month, MonitoringNight, Date, Time, Sunrise, Sunset, `Elapsed (hr)`, Species,
+          Timestamp, Temp, `Month Ind`)
+
+# Fill temperature values
+# Get nearest NOAA stations for temperature fill, test individually
+noaas <- noaa_nearby(lat, lon)
+noaa_station <- "USW00094290"
+
+temps <- noaa_daily(noaa_station, as.character(min(zd_df$Date)),
+                    as.character(max(zd_df$Date)),
+                    datatypes = "TMIN") %>%
+    select(date, tmin) %>%
+    rename(Date = date)
+
+zd_temp <- zd_df %>%
+    left_join(temps, by = join_by(Date)) %>%
+    mutate(Temp = coalesce(Temp, as.character(tmin))) %>%
+    select(-tmin)
 
 # Save to XLSX
+#xlsx_df <- zd_df %>% 
+#    mutate(across(where(is.character), ~replace_na(., "")))
 
 book_path <- file.path(getwd(), "xlsx_files", "ZooData2026.xlsx")
 wb <- wb_load(book_path)
 sheet_name <- "2026Data"
 exist_rows <- nrow(wb_read(wb, sheet_name, col_names = FALSE))
-wb <- wb_add_data(wb, sheet_name, zd_df,
-                  start_row = exist_rows + 1, col_names = FALSE)
+#wb <- wb_add_data(wb, sheet_name, zd_df,
+#                  start_row = exist_rows + 1, col_names = FALSE,
+#                  na = "")
+wb <- wb_add_data(wb, sheet_name, zd_temp, na = "")
 #wb <- wb_add_numfmt(wb, sheet_name,
 #                    dims = wb_dims(
 #                        rows = (exist_rows + 1):(exist_rows + nrow(zd_df)),
@@ -91,7 +136,7 @@ wb <- wb_add_data(wb, sheet_name, zd_df,
 #                    numfmt = 20)
 wb <- wb_set_col_widths(wb,
                         sheet = sheet_name,
-                        cols = 1:ncol(zd_df),
+                        cols = 1:ncol(zd_temp),
                         widths = "auto")
 new_path <- file.path(getwd(), "xlsx_files", 
                      paste0("ZooData2026_", Sys.Date(), ".xlsx"))
